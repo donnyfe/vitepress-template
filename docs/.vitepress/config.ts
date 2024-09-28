dfc7d7ca09f9456be4721a4182a0f07f130c5d64
donny <donnyfe@qq.com>
2024-09-28 13:15:23 +0800

    build: 更新依赖

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -9,41 +9,30 @@ export default ({ mode }) => {
 
   return defineConfig({
     base,
-
-    title: "Wiki",
-
-    description: "知识库",
-
+    title: "Vitepress",
+    description: "文档库",
     // Use git commit to get the timestamp
     lastUpdated: true,
-
     // When set to true, VitePress will not fail builds due to dead links.
     ignoreDeadLinks: true,
-
     head: [
       [
         "link",
         { rel: "icon", type: "image/svg+xml", href: "/assets/logo.svg" },
       ],
     ],
-
     themeConfig: {
       nav,
-
       sidebar,
-
       outlineTitle: "快速前往",
-
       socialLinks: [
         { icon: "github", link: "https://github.com/vuejs/vitepress" },
       ],
-
       footer: {
         message: "Released under the MIT License.",
         copyright: "Copyright © 2024-present",
       },
     },
-
     // docs: https://vitepress.vuejs.org/config/app-configs.html#markdown
     markdown: {
       theme: "github-dark",
